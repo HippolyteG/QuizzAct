a3c7c31b6d48ef813d165550edd31a54d8750b7d
Mathis <mathis.bremont@etu.unilim.fr>
2020-03-12 10:52:24 +0100

    Page accueil

--- app/src/main/java/com/example/quizzact/MainActivity.java
+++ app/src/main/java/com/example/quizzact/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.quizzact;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.buttonPlay = (Button) findViewById(R.id.buttonPlay);
+
+        this.buttonPlay.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,QuizzActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
