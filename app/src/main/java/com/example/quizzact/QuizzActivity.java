7703029857ba88ffa649ed0e8485a03568a425db
Hippolyte Guillemeteau <hippolyte.guillemeteau@etu.unilim.fr>
2020-03-12 10:32:40 +0100

    quizz java part1

---
+++ app/src/main/java/com/example/quizzact/QuizzActivity.java
@@ -0,0 +1,12 @@
+package com.example.quizzact;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuizzActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizz);
+    }
+}
